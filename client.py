78bdd907aeb1eda47a81de9427cfdfb1994ee733
Shihab Islam <shihabhunterislam@gmail.com>
2022-09-25 19:22:21 -0400

    do not need main.py

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ import time
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.host = "localhost"
+        self.host = "173.77.106.71"
         self.port = 5555
         self.addr = (self.host, self.port)
         self.board = self.connect()
