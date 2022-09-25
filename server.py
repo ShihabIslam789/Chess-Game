5a2dd74dbca111d0ac9f58862081a39dd4c2f19d
Shihab Islam <shihabhunterislam@gmail.com>
2022-09-25 11:42:08 -0400

    initiale server connection files.

---
+++ server.py
@@ -0,0 +1,18 @@
+import socket
+from _thread import *
+from board import Board
+import pickle
+import time
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server = "localhost"
+port = 5555
+
+server_ip = socket.gethostbyname(server)
+
+try:
+    s.bind((server, port))
+
+except socket.error as e:
+    print(str(e))
